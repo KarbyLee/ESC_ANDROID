7ea17480b40ce4299b27b91af64d42da44183e32
NotHyoone <gydnjs3505@gmail.com>
2026-05-08 18:10:38 +0900

    feat: 2주차 실습 파일 업로드

--- hyowon/MYAPP1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ hyowon/MYAPP1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,44 +4,73 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.checkScrollableContainerConstraints
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen(this)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(mainActivity: MainActivity) {
+    var message by remember { mutableStateOf("안녕") }
+    var count by remember { mutableStateOf(0) }
+    var buttonColor by remember { mutableStateOf(Color.Blue)}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = message, fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(text = "클릭 횟수: $count", fontSize = 18.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+           onClick = {
+               count++
+               if (count == 100) {
+                   buttonColor = Color.Blue
+                   count = 0
+                   message ="너무 많이 누르지 마"
+               } else if (count == 46) {
+                   buttonColor = Color.Red
+                   message = "내 학번 끝 2자리:46"
+               } else {
+                   buttonColor = Color.Blue
+                   message = "버튼 클릭됨"
+               }
+           } ,
+            colors = buttonColors(containerColor = buttonColor)
+        ) {
+            Text(text = "클릭")
+        }
     }
+
 }
